ae85fbcd0a697b37875cdc185dff199c20e01142
fgsantosti <fgsantos.ti@gmail.com>
2019-06-22 14:22:57 -0300

    Update agenda.py

--- Cap 09/agenda.py
+++ Cap 09/agenda.py
@@ -30,24 +30,48 @@ def ler():
         agenda.append([nome, telefone])
     arquivo.close()
 
-    print ("\nAgenda\n\n------")
+    print ("\nContatos da Agenda\n\n------")
+    i = 0
     for e in agenda:
-        print ("Nome: %s Telefone: %s" % (e[0], e[1]))
+        print (i," - Nome: %s Telefone: %s" % (e[0], e[1]))
+        i = i + 1
+
+def excluir():
+    arquivo = open("agenda.txt", "r", encoding="utf-8")
+    agenda = []
+    for l in arquivo.readlines():		
+        nome, telefone = l.strip().split("#")
+        agenda.append([nome, telefone])
+    arquivo.close()
+
+    ler()
+    idcontato = int(input('Digite o id do contato:'))
+    del agenda[idcontato]
+
+    #gravando no arquivo agenda.txt com o modo w
+    arquivo = open("agenda.txt", "w", encoding="utf-8")
+    for e in agenda:
+        arquivo.write("%s#%s\n" % (e[0], e[1]))
+    arquivo.close()
+    
     
 def menu():
     print ("""
         1 - Novo
         2 - Ler
+        3 - Excluir
         0 - Sai
     """)
     op = int(input("Escolha uma opção: "))
     return op
 
 while True:
-    opção = menu()
-    if opção == 0:
+    opcao = menu()
+    if opcao == 0:
         break
-    elif opção == 1:
+    elif opcao == 1:
         novo()
-    elif opção == 2:
+    elif opcao == 2:
         ler()
+    elif opcao == 3:
+        excluir()
